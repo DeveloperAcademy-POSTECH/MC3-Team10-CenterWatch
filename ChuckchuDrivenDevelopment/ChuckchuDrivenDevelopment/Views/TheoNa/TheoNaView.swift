6fadd1c507bf19be0e263c794be83930529e3786
Ye Eun Lianne Choi <yenchoi010@gmail.com>
2023-07-19 17:03:06 +0900

    [Chore] 작업 브랜치 머지 및 충돌 해결

---
+++ ChuckchuDrivenDevelopment/ChuckchuDrivenDevelopment/Views/TheoNa/TheoNaView.swift
@@ -0,0 +1,20 @@
+//
+//  TheoNaView.swift
+//  ChuckchuDrivenDevelopment
+//
+//  Created by Ye Eun Choi on 2023/07/19.
+//
+
+import SwiftUI
+
+struct TheoNaView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TheoNaView_Previews: PreviewProvider {
+    static var previews: some View {
+        TheoNaView()
+    }
+}

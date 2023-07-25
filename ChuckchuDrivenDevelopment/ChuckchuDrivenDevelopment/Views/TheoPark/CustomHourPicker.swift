952824d385b8675fda201168a4dc51d8b878cc6c
Ye Eun Lianne Choi <yenchoi010@gmail.com>
2023-07-26 01:25:24 +0900

    [Style] Picker 폰트 수정

--- ChuckchuDrivenDevelopment/ChuckchuDrivenDevelopment/Views/TheoPark/CustomHourPicker.swift
+++ ChuckchuDrivenDevelopment/ChuckchuDrivenDevelopment/Views/TheoPark/CustomHourPicker.swift
@@ -14,15 +14,12 @@ struct CustomHourPicker: View {
         Picker("", selection: $selectedHour) {
             ForEach(0..<24) { hour in
                 Text("\(hour):00").tag(hour)
-                    .font(.system(size: 18))
+                    .font(.system(size: 20))
+                    .fontWeight(.semibold)
+                    .foregroundColor(.blue) // FIXME: 추후에 accentColor로 색상 변경
             }
         }
+        .padding(.trailing, -8)
         .pickerStyle(WheelPickerStyle())
     }
 }
-
-//struct CustomHourPicker_Previews: PreviewProvider {
-//    static var previews: some View {
-//        CustomHourPicker()
-//    }
-//}

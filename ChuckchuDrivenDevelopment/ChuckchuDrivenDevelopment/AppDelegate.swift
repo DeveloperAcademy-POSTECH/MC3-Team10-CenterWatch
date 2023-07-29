f550165424b2ba4ae23f6fbbd4d317541f39778b
Ye Eun Lianne Choi <yenchoi010@gmail.com>
2023-07-29 23:55:48 +0900

    [Feat] 다음 요일의 알림을 생성하는 Background tasks 추가

--- ChuckchuDrivenDevelopment/ChuckchuDrivenDevelopment/AppDelegate.swift
+++ ChuckchuDrivenDevelopment/ChuckchuDrivenDevelopment/AppDelegate.swift
@@ -6,13 +6,46 @@
 //
 
 import SwiftUI
+import BackgroundTasks
 
-class AppDelegate: NSObject, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate {
     @ObservedObject var localNotificationManager: LocalNotificationManager = LocalNotificationManager()
+   
+    let calendar = Calendar.current
+    
+    var weekdays: [Int] = UserDefaults.standard.array(forKey: "notificationWeekdays") as? [Int] ?? []
+    var startHour: Int = UserDefaults.standard.integer(forKey: "notificationStartHour")
+    var endHour: Int = UserDefaults.standard.integer(forKey: "notificationEndHour")
+    var frequency: Int = UserDefaults.standard.integer(forKey: "notificationFrequency")
 
+    
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    
+        
+        /// background fetch 관련 작업을 등록
+        /// 앱이 꺼지면 task scheduler를 등록하고 refresh를 스케줄한다
+        BGTaskScheduler.shared.register(forTaskWithIdentifier: UUID().uuidString,
+                                        using: nil) { task in
+            task.setTaskCompleted(success: true)
+            /// 요일이 바뀌면, 현재 요일과 userDefaults에 저장된 요일 데이터 비교
+            /// 현재 요일이 데이터에 포함될 시 새로운 알림 리스트 생성
+            let currentWeekday = getCurrentWeekday()
+            
+            for weekday in self.weekdays {
+                if weekday == currentWeekday {
+                    self.localNotificationManager.setLocalNotification(
+                        weekday: weekday,
+                        startHour: self.startHour,
+                        endHour: self.endHour,
+                        frequency: MinuteInterval(rawValue: self.frequency) ?? .hour)
+                   
+                    self.scheduleAppRefresh() // 다음 background refresh 예약
+                }
+            }
+        }
+        scheduleAppRefresh() // FIXME: 이게 왜 한 번 더 들어가야 하는 거지?
+        
+        /// 시스템 알림 허용이 됐는지 구분
         if localNotificationManager.isAuthorizationRequested {
             if #available(iOS 10.0, *) {
                 UNUserNotificationCenter.current().delegate = self as UNUserNotificationCenterDelegate
@@ -37,10 +70,33 @@ class AppDelegate: NSObject, UIApplicationDelegate {
        
         return true
     }
+    
+    
 }
 
 
-// MARK: - 알람 처리
+
+// MARK: - Background fetch 핸들링 관련
+extension AppDelegate {
+    /// BGAppRefreshTaskRequest 생성 후 BGTaskScheduler.submit(_:)을 통해 request 발송
+    func scheduleAppRefresh() {
+        /// 현재 요일을 파악하기 위해, 매일 자정에 background refresh가 요청 됨
+        let dateComponents = DateComponents(hour: 0, minute: 0, second: 0)
+        let refreshDate = calendar.date(from: dateComponents)
+        let request = BGAppRefreshTaskRequest(identifier: UUID().uuidString)
+        request.earliestBeginDate = refreshDate
+        do {
+            try BGTaskScheduler.shared.submit(request)
+            print("background refresh scheduled")
+        } catch {
+            print("Couldn't schedule app refresh \(error.localizedDescription)")
+        }
+    }
+}
+
+
+
+// MARK: - 알림 액션 핸들링 관련
 extension AppDelegate: UNUserNotificationCenterDelegate {
 
     /// in Foreground
@@ -67,6 +123,4 @@ extension AppDelegate: UNUserNotificationCenterDelegate {
         completionHandler()
     }
 }
-
-
 

33f45f3cc6ba9685c0e609a2578ba206359627bf
Ye Eun Lianne Choi <yenchoi010@gmail.com>
2023-08-06 00:07:04 +0900

    [Feat] 온보딩 분기처리 추가

--- ChuckchuDrivenDevelopment/ChuckchuDrivenDevelopment/ChuckchuDrivenDevelopmentApp.swift
+++ ChuckchuDrivenDevelopment/ChuckchuDrivenDevelopment/ChuckchuDrivenDevelopmentApp.swift
@@ -10,32 +10,45 @@ import SwiftUI
 
 @main
 struct ChuckchuDrivenDevelopmentApp: App {
-    
-    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     @AppStorage("isDarkModeEnabled") var isDarkModeEnabled = true // 다크 모드 상태를 저장하는 변수
+    @AppStorage("firstInApp") var ifFirstInApp = true // 앱 첫 진입 여부를 저장하는 변수
     @State private var isLoading: Bool = false // 스플릿뷰를 부르는 변수
-    
     
     var body: some Scene {
         WindowGroup {
             Group {
-                ZStack {
-                    MainView()
-                    
-                    SplashView()
-                        .opacity(isLoading ? 0 : 1)
-                        .onAppear {
-                            DispatchQueue.main.asyncAfter(deadline: .now() + 2.5) {
-                                withAnimation(.easeInOut(duration: 2)) {
-                                    isLoading.toggle()
+                if ifFirstInApp == true {
+                    NavigationStack {
+                        OnboardingView1()
+                    }
+                } else {
+                    ZStack {
+                        MainView()
+
+                        SplashView()
+                            .opacity(isLoading ? 0 : 1)
+                            .onAppear {
+                                DispatchQueue.main.asyncAfter(deadline: .now() + 2.5) {
+                                    withAnimation(.easeInOut(duration: 2)) {
+                                        isLoading.toggle()
+                                    }
                                 }
                             }
-                        }
+                    }
                 }
             }
             .preferredColorScheme(isDarkModeEnabled ? .dark : .light) // 다크 모드 상태에 따라 컬러 스킴 설정
-            
-            
         }
     }
+
+}
+
+
+
+// MARK: - Check If First In App (Method)
+/// 온보딩뷰를 적절히 보여주기 위해, 사용자가 앱을 처음 진입했는지의 여부를 저장합니다.
+public func checkIfFirstInApp() {
+    UserDefaults.standard.set(false, forKey: "firstInApp")
+    print("firstInApp >>>>> ", UserDefaults.standard.bool(forKey: "firstInApp"))
 }

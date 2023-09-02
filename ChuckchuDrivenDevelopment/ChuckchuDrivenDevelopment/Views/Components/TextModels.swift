655c0ef4c8432f865e0a8c1b7ca40b709fc2ea88
SingJn <33142878+SingJn@users.noreply.github.com>
2023-09-02 16:34:45 +0900

    [Feat] Move Default Language To English
    - 기본 언어를 영어로 변경합니다.
    - TextModels.swift 파일을 새로 만들고, 앱 내부 모든 문자를 옮겼습니다.

---
+++ ChuckchuDrivenDevelopment/ChuckchuDrivenDevelopment/Views/Components/TextModels.swift
@@ -0,0 +1,187 @@
+//
+//  TextModels.swift
+//  ChuckchuDrivenDevelopment
+//
+//  Created by 박상준 on 2023/09/02.
+//
+
+import SwiftUI
+
+public final class AppText {
+    
+    //File Name
+        //Method Name
+    class LocalNotificationManager {
+        class MakeNotificationContent {
+            let title = "GgoodGgood"
+            let body = "Straighten your back🐢"
+            let infoKey = "Straighten Fynn"
+            let infoValue = "Fynn"
+        }
+        class SetNextDayNotification {
+            let title = "The promised day has passed!"
+            let body = "Reset the notification and receive Fynn's message again.🐢"
+            let infoKey = "Straighten Fynn"
+            let infoValue = "Fynn"
+        }
+    }
+    
+    class NotificationTitle {
+        class OpeningVariations {
+            let stringArray = [
+                "Another lively day today! I'll give you strength! 🐢"
+            ]
+        }
+        class Variations {
+            let stringArray = [
+                "Knock-knock, it's the spine fairy 🧚",
+                "I'm naturally a turtle neck, but what about you… 🐢",
+                "Save on back surgery costs with just one stretch! 💸",
+                "How about some stretching for your hardworking back? 💪",
+                "Straighten that back! Stretch it out 🌈",
+                "Find my notifications annoying? Wait until your back hurts... 🔥",
+                "A healthy spine leads to a healthy mind! 😌",
+                "Haha! Master, be careful not to hunch like me 🐢",
+                "Keep both your back and spine strong! 🎋",
+                "Stretch it out right from your seat! 🙌",
+                "Give your shoulders a twist~ 💞",
+                "More efficient work with a healthy spine! 💪🦋",
+                "Put some power into your back even during work! 👊",
+                "Good posture, a happy day! 🌟🧘‍♂️",
+                "Check your posture to prevent turtle neck! 🐢👀",
+                "Turtle neck, the fate of developers? No! 👊",
+                "Great ideas come from a healthy spine 💡",
+                "If your posture is hunched, dolphins might mistake you for a friend 🐬",
+                "Oh my, your neck might snap! 💥",
+                "Give your wrists a refreshing twist now and then ⚡️",
+                "Straighten your back firmly 🥢",
+                "Oops! Straighten your spine before it screams in agony 😱",
+                "Fix your posture with a fresh heart 🌷",
+                "My back... save me...! Good posture, stand firm! ⚰️",
+                "Check your posture, ready, set, go! 🏃💨",
+                "Fix your posture once and power up! 🔥",
+                "Oh? You're hunched. Stretch right away! 🐬",
+                "Stretch your arms and legs out! Preventing turtle neck is a must! 🐢💪",
+                "They say 10 seconds of good posture can change 10 years 😘",
+                "Good posture takes responsibility for spinal health! 💪",
+                "Master, what if your posture is worse than mine 😥"
+            ]
+        }
+        class ClosingVariations {
+            let stringArray = [
+                "Well done! Let's meet with energy again tomorrow! 👋🏻"
+            ]
+        }
+    }
+    
+    class OnBoardingView1 {
+        class OnboardingText {
+            let title = "Creating Good Posture\nwith Pin"
+            let subTitle = "At your preferred intervals, receive notifications and easily achieve proper posture instantly."
+        }
+        class NextButton {
+            let label = "Getting started"
+        }
+    }
+
+    class OnBoardingView2 {
+        class IconAndText {
+            let textLabel = "If you allow notifications, you can receive heartfelt messages from Fynn."
+        }
+        class SettingButton {
+            let label = "Notification Settings"
+        }
+    }
+    
+    class DailyNotiToggleRow {
+        class ToggleButton {
+            let label = "Turn off notifications for a day"
+        }
+    }
+    
+    class MainView {
+        class PleaseTurnOnTheNotiView {
+            let message = "Oh...! Fynn wants to send a message. Activation requires notification settings."
+            let systemSeting = "System Settings"
+        }
+    }
+    
+    class SettingInformationCell {
+        class SettingModalButton {
+            let name = "Notification Settings"
+        }
+        class SelectedFrequncyViewer {
+            let title = "Notification Interval"
+            let unit = "Hour"
+        }
+        class SelectedHourViewer {
+            let startTitle = "Start Time"
+            let endTitle = "End Time"
+        }
+        class SelectedDayViewer {
+            let title = "Day"
+            let mon = "M"
+            let tue = "T"
+            let wed = "W"
+            let thu = "T"
+            let fri = "F"
+        }
+    }
+    
+    class IfDailyNotiOffCell {
+        class View {
+            let message = "The notifications are currently turned off. Fynn will be back tomorrow."
+        }
+    }
+    
+    class ModalView {
+        class FrequencySettingRow {
+            let label = "Notification Settings"
+        }
+        class SelectedDayRow {
+            let label = "Day"
+        }
+        class NaviationTitle {
+            let label = "Notification Settings"
+            let cancel = "Cancel"
+            let complete = "Confirm"
+        }
+    }
+    
+    class FrequencySettingRow {
+        class Picer {
+            let unit = "Hour"
+        }
+    }
+    
+    class TimePickerRow {
+        class View {
+            let startLabel = "Start Time"
+            let endLabel = "End Time"
+        }
+    }
+    
+    class DataModels {
+        class SelectedDays {
+            let sun = "S"
+            let mon = "M"
+            let tue = "T"
+            let wed = "W"
+            let thu = "T"
+            let fri = "F"
+            let sat = "S"
+        }
+    }
+    
+    class CharacterAnimation {
+        class EasterEgg {
+            let team = "Watching for a watch in the center of the world"
+            let guardy = "Guardy / SuHo Kim"
+            let theoPark = "Theo(Park) / SangJun Park"
+            let lianne = "Lianne / YeEun Choi"
+            let alex = "Alex / DoHu Lee"
+            let theoNa = "Theo(Na) / GyeongBin Na"
+        }
+    }
+    
+}
